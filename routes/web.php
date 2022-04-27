15af87d8b37aa34786bb14e6b84f771472fd5e04
Fernando Kramar <macedokramar@gmail.com>
2022-04-27 16:53:57 -0300

    plan listing

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PlanController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Route::get('admin/plans', 'Controllers\Admin\PlanController@index')->name('plans.index');
+
+Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
 
 Route::get('/', function () {
     return view('welcome');
